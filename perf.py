1a55ef47e0b6d09d896ecabccee9d856bfe68ef3
wujiabin <wujiabin@baidu.com>
2015-11-03 14:22:22 +0800

    add perf

--- perf.py
+++ perf.py
@@ -7,8 +7,33 @@ import timeit
 sys.path.append('./py_version')
 sys.path.append('./newtype_version')
 
-print timeit.timeit('ip.get_country("180.214.232.50")', 'import _iputils; ip = _iputils.IP2Location("dict/czip.txt")')
-print timeit.timeit('ip.get_country("180.214.232.50")', '''
+def timeit_wrapper(name, stmt, setup, number=1000000):
+    print "%s: %.4f/%d s" % (name, timeit.timeit(stmt, setup, number=number), number)
+
+print "load dict perf:"
+
+timeit_wrapper('c ext load dict', '_iputils.IP2Location("dict/czip.txt")', 'import _iputils', number=10)
+timeit_wrapper('c ext load dict with user-defined parse', '_iputils.IP2Location("dict/cz88ip.txt", parse)', '''
+import _iputils
+
+def parse(line):
+    fields = line.split()
+    return tuple(fields[:3])
+''', number=10)
+
+timeit_wrapper('py load dict', 'iputils.IP2Location("dict/czip.txt")', 'import iputils', number=10)
+timeit_wrapper('py load dict with user-defined parse', 'iputils.IP2Location("dict/cz88ip.txt", parse)', '''
+import iputils
+
+def parse(line):
+    fields = line.split()
+    return tuple(fields[:3])
+''', number=10)
+
+print "query perf:"
+
+timeit_wrapper('c ext query', 'ip.get_country("180.214.232.50")', 'import _iputils; ip = _iputils.IP2Location("dict/czip.txt")')
+timeit_wrapper('c ext query(load dict with user-defined parse)', 'ip.get_country("180.214.232.50")', '''
 import _iputils
 
 def parse(line):
@@ -18,4 +43,14 @@ def parse(line):
 ip = _iputils.IP2Location("dict/cz88ip.txt", parse)
 ''')
 
-print timeit.timeit('ip.get_country("180.214.232.50")', 'import iputils; ip = iputils.IP2Location("dict/czip.txt")')
+timeit_wrapper('py query', 'ip.get_country("180.214.232.50")', 'import iputils; ip = iputils.IP2Location("dict/czip.txt")')
+timeit_wrapper('py query(load dict with user-defined parse)', 'ip.get_country("180.214.232.50")', '''
+import iputils
+
+def parse(line):
+    fields = line.split()
+    return tuple(fields[:3])
+
+ip = iputils.IP2Location("dict/cz88ip.txt", parse)
+''')
+
